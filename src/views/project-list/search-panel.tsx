38a5bc791e1f2b1a673ccb8f282c1929032511f1
冯天石 <fengtianshi@bytedance.com>
2022-07-18 18:13:55 +0800

    feat: 重构为ts代码

--- src/views/project-list/search-panel.tsx
+++ src/views/project-list/search-panel.tsx
@@ -1,10 +1,26 @@
-export const SearchPanel = ({ users, param, setParam }) => {
+export interface User {
+  id: string;
+  name: string;
+  email: string;
+  title: string;
+  organization: string;
+}
+
+interface SearchPanelProps {
+  users: User[];
+  param: {
+    name: string;
+    personId: string;
+  };
+  setParam: (param: SearchPanelProps['param']) => void;
+}
+export const SearchPanel = ({ users, param, setParam }: SearchPanelProps) => {
   return (
     <form action=''>
       <div>
         <input
           type='text'
-          value={param.value}
+          value={param.name}
           onChange={(e) => setParam({ ...param, name: e.target.value })}
         />
         <select
