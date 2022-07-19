ad6c9e156b0ce8bf0c2d1b878e052f15a920afcb
冯天石 <fengtianshi@bytedance.com>
2022-07-19 11:23:43 +0800

    feat: 登录设计，手写表单。

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { ProjectList } from 'views/project-list';
 import { TsReactTest } from './test/try-use-array';
+import { Login } from './views/login';
 
 function App() {
   return (
     <div className='App'>
-      <ProjectList />
-      <TsReactTest />
+      {/*<ProjectList />*/}
+      {/*<TsReactTest />*/}
+      <Login />
     </div>
   );
 }
